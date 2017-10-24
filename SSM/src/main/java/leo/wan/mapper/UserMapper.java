e0f05dbb07ce84a4e29949908ac9cb9408e6b906
leo-wan0225 <wjing225@163.com>
2017-10-24 21:53:23 +0800

    同步一些修改

--- SSM/src/main/java/leo/wan/mapper/UserMapper.java
+++ SSM/src/main/java/leo/wan/mapper/UserMapper.java
@@ -1,11 +1,14 @@
 package leo.wan.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 
 import leo.wan.entity.User;
-
+@Repository
 public interface UserMapper {
-	User findUserByNameAndPwd(@Param("id2")int id,@Param("pwd")String pwd);
+	List<User> findUserByPage(Map<String, Object> map);
+	Integer getTotalUserCount(Map<String, Object> map);
 }
