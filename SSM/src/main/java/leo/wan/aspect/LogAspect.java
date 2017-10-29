222adbdc934a01a7a88987cb708fc742d6cecf4c
leo-wan0225 <wjing225@163.com>
2017-10-29 11:35:04 +0800

    加上日志管理

--- SSM/src/main/java/leo/wan/aspect/LogAspect.java
+++ SSM/src/main/java/leo/wan/aspect/LogAspect.java
@@ -1,16 +1,30 @@
 package leo.wan.aspect;
 
+import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggerFactory;
+import org.aspectj.lang.ProceedingJoinPoint;
 
 
 /**
  * 
  *Title: 
  *Description:利用aop的方式实现日志的统一处理
- *Company: z
+ *Company: 
  * @author Administrator
  * @date 2017年8月29日  上午11:17:13
  */
 public class LogAspect {
 	Logger logger = Logger.getLogger(LogAspect.class);
+	public Object loggingAround(ProceedingJoinPoint jp){
+		
+		
+		//调用目标方法
+		try {
+			Object object = jp.proceed();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+		return "";
+	}
 }
--- SSM/src/main/java/leo/wan/log/LogAppender.java
+++ SSM/src/main/java/leo/wan/log/LogAppender.java
@@ -6,7 +6,7 @@ import org.apache.log4j.Priority;
 /**
  * 
  *Title: 
- *Description:实现log日志的按级别分类存放
+ *Description:实现log日志的按级别分类存放,如果要实现按级别存放日志，则需要在log的配置文件中引用该类的路径即可
  *Company: 
  * @author Administrator
  * @date 2017年8月28日  下午11:07:04
