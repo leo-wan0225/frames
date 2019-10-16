2c66042581e02908578db29faa4167514f0442f9
wjing225@163.com <wjing225@163.com>
2019-10-16 22:18:11 +0800

    11

--- SSM/src/main/java/leo/wan/service/UserService.java
+++ SSM/src/main/java/leo/wan/service/UserService.java
@@ -3,17 +3,17 @@ package leo.wan.service;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
 
 import leo.wan.annotation.LogDescribe;
 import leo.wan.model.User;
 import leo.wan.dao.UserMapper;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
 public class UserService {
-	@Resource
+	@Autowired
 	private UserMapper userMapper;
 	@LogDescribe(description="查询用户信息")
 	public List<User> findUserByPage(Map<String, Object> map){
